e529cbb258739989a84dd3f5f3aba3812ab10008
H-888 <1370438360@qq.com>
2020-10-09 19:52:51 +0800

    下载路由,redux,axios包

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-ReactDOM.render("Hello world",document.getElementById('root'));
+import App from './App'
+ReactDOM.render(<App/>,document.getElementById('root'));
 
